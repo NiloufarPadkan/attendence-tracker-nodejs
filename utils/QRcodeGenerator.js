cd1bc43a9719c1317babcc4b725c3f5a6c743ee1
Niloufar padkan <niloufar.pa@gmail.com>
2022-07-15 23:39:19 +0430

    QRCode generator updated

--- utils/QRcodeGenerator.js
+++ utils/QRcodeGenerator.js
@@ -1,21 +1,14 @@
 const QRCode = require("qrcode");
 
 function QRcodeGenerator(data, fileName) {
-  QRCode.toFile(
-    `QRcode_files/${fileName}.png`,
-    data,
-    {
-      // color: {
-      //   dark: "#00F", // Blue dots
-      //   light: "#0000", // Transparent background
-      // },
-      scale: 5,
-    },
-    function (err) {
-      if (err) throw err;
-      return `QRcode_files/${fileName}.png`;
-    }
-  );
+  QRCode.toFile(`QRcode_files/${fileName}.png`, data, {
+    // color: {
+    //   dark: "#00F", // Blue dots
+    //   light: "#0000", // Transparent background
+    // },
+    scale: 5,
+  });
+  return `QRcode_files/${fileName}.png`;
 }
 
 module.exports.QRcodeGenerator = QRcodeGenerator;

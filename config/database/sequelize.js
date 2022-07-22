1a24abe725c0993fec0c97c87778aed59b26cb08
Niloufar padkan <niloufar.pa@gmail.com>
2022-07-22 20:38:21 +0330

    database typo fixed

--- config/database/sequelize.js
+++ config/database/sequelize.js
@@ -1,12 +1,12 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("attendence-tracker", "root", "", {
-    dialect: "mysql",
-    port: process.env.SQL_PORT,
-    host: "localhost",
-    charset: "utf8",
-    collate: "utf8_persian_ci",
-    logging: false,
+const sequelize = new Sequelize("attendance_tracker", "root", "", {
+  dialect: "mysql",
+  port: process.env.SQL_PORT,
+  host: "localhost",
+  charset: "utf8",
+  collate: "utf8_persian_ci",
+  logging: false,
 });
 
 module.exports = sequelize;

a4b4e292a4e12cf2c3f2a275471f5877eaec6d28
Niloufar padkan <niloufar.pa@gmail.com>
2022-07-27 14:21:04 +0330

    minot problem fixed

--- app/employee/service/attendance/attendanceHistory.js
+++ app/employee/service/attendance/attendanceHistory.js
@@ -4,16 +4,51 @@ const Employee = require("../../../../models/Employee");
 const AttendanceRecords = require("../../../../models/AttendanceRecords");
 const Op = Sequelize.Op;
 
+function calculatePresense(
+  history,
+  presenceDuration,
+  scheduledPresenseduration,
+  currentDate
+) {
+  history.forEach((element) => {
+    if (element.checkInTime < element.endTime) {
+      //scheduled presense calculation
+      if (!element.checkOutTime) {
+        scheduledPresenseduration += moment
+          .duration(currentDate.format("HH:mm:ss"))
+          .subtract(moment.duration(element.checkInTime));
+      } else if (element.checkOutTime > element.endTime) {
+        scheduledPresenseduration += moment
+          .duration(element.endTime)
+          .subtract(moment.duration(element.checkInTime));
+      } else {
+        scheduledPresenseduration += moment
+          .duration(element.checkOutTime)
+          .subtract(moment.duration(element.checkInTime));
+      }
+    }
+    if (!element.checkOutTime) {
+      presenceDuration += moment
+        .duration(currentDate.format("HH:mm:ss"))
+        .subtract(moment.duration(element.checkInTime));
+    } else {
+      presenceDuration += moment
+        .duration(element.checkOutTime)
+        .subtract(moment.duration(element.checkInTime));
+    }
+  });
+  return { presenceDuration, scheduledPresenseduration };
+}
 exports.dailyHistory = async (req) => {
   let employeeId = req.Employee.id;
   var currentDate = moment(new Date()); //current full Date
   var currentTime = moment(currentDate).format("HH:mm:ss"); //current full Date
-  var weekDayName = moment(currentDate).format("dddd"); //current day of week
+  var currentDayName = moment(currentDate).format("dddd"); //current day of week
   const beginningOfDay = moment(currentDate, "YYYY-MM-DD").startOf("day");
   const endOfDay = moment(currentDate, "YYYY-MM-DD").endOf("day");
   let record = { presence: "00:00:00", absence: "00:00:00", delay: "00:00:00" };
   let presenceDuration = 0;
-  let scheduledPresense = 0;
+  let scheduledPresenseduration = 0;
   let absenseDuration = 0;
   let delayDuration = 0;
 
@@ -24,7 +59,7 @@ exports.dailyHistory = async (req) => {
   });
   if (!employee) return "employeeNotFound";
   let workDays = employee.workdays;
-  if (workDays.includes(weekDayName)) {
+  if (workDays.includes(currentDayName)) {
     let history = await AttendanceRecords.findAll({
       where: {
         employeeId: employeeId,
@@ -47,43 +82,25 @@ exports.dailyHistory = async (req) => {
         moment(history[0].startTime, "HH:mm:ss")
       );
     }
+
     let delay = moment(history[0].checkInTime, "HH:mm:ss").diff(
       moment(history[0].startTime, "HH:mm:ss")
     );
 
     record.delay = moment.utc(delay).format("HH:mm:ss");
+    let presenseCalculation = calculatePresense(
+      history,
+      presenceDuration,
+      scheduledPresenseduration,
+      currentDate
+    );
 
-    history.forEach((element) => {
-      if (element.checkInTime < element.endTime) {
-        //scheduled presense calculation
-        if (!element.checkOutTime) {
-          scheduledPresense += moment
-            .duration(currentDate.format("HH:mm:ss"))
-            .subtract(moment.duration(element.checkInTime));
-        } else if (element.checkOutTime > element.endTime) {
-          scheduledPresense += moment
-            .duration(element.endTime)
-            .subtract(moment.duration(element.checkInTime));
-        } else {
-          scheduledPresense += moment
-            .duration(element.checkOutTime)
-            .subtract(moment.duration(element.checkInTime));
-        }
-      }
-      if (!element.checkOutTime) {
-        presenceDuration += moment
-          .duration(currentDate.format("HH:mm:ss"))
-          .subtract(moment.duration(element.checkInTime));
-      } else {
-        presenceDuration += moment
-          .duration(element.checkOutTime)
-          .subtract(moment.duration(element.checkInTime));
-      }
-      absenseDuration += moment(workTime).diff(moment(scheduledPresense));
-    });
-    // presenceDuration = moment.duration(presenceDuration);
-    // scheduledPresense = moment.duration(scheduledPresense);
-    record.presence = moment.utc(presenceDuration).format("HH:mm:ss");
+    absenseDuration = moment(workTime).diff(
+      moment(presenseCalculation.scheduledPresenseduration)
+    );
+    record.presence = moment
+      .utc(presenseCalculation.presenceDuration)
+      .format("HH:mm:ss");
     record.absence = moment.utc(absenseDuration).format("HH:mm:ss");
     return record;
   }
@@ -99,7 +116,7 @@ exports.getHistoryByDate = async (req) => {
   let presenceDuration = 0;
   let absenseDuration = 0;
   let delayDuration = 0;
-  let scheduledPresense = 0;
+  let scheduledPresenseduration = 0;
 
   let employee = await Employee.findOne({
     where: {
@@ -154,34 +171,17 @@ exports.getHistoryByDate = async (req) => {
         .duration(history[0].checkInTime)
         .subtract(moment.duration(history[0].startTime));
 
-      history.forEach((element) => {
-        if (element.checkInTime < element.endTime) {
-          //scheduled presense calculation
-          if (!element.checkOutTime) {
-            scheduledPresense += moment
-              .duration(date.format("HH:mm:ss"))
-              .subtract(moment.duration(element.checkInTime));
-          } else if (element.checkOutTime > element.endTime) {
-            scheduledPresense += moment
-              .duration(element.endTime)
-              .subtract(moment.duration(element.checkInTime));
-          } else {
-            scheduledPresense += moment
-              .duration(element.checkOutTime)
-              .subtract(moment.duration(element.checkInTime));
-          }
-        }
-        if (!element.checkOutTime) {
-          presenceDuration += moment
-            .duration(date.format("HH:mm:ss"))
-            .subtract(moment.duration(element.checkInTime));
-        } else {
-          presenceDuration += moment
-            .duration(element.checkOutTime)
-            .subtract(moment.duration(element.checkInTime));
-        }
-        absenseDuration += moment(workTime).diff(moment(scheduledPresense));
-      });
+      let presenseCalculation = calculatePresense(
+        history,
+        presenceDuration,
+        scheduledPresenseduration,
+        currentDate
+      );
+      presenceDuration = presenseCalculation.presenceDuration;
+      scheduledPresenseduration = presenseCalculation.scheduledPresenseduration;
+      absenseDuration += moment(workTime).diff(
+        moment(scheduledPresenseduration)
+      );
     }
   }
   record.presence = moment.utc(presenceDuration).format("HH:mm:ss");
